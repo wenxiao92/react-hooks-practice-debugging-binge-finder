6202377658a2371a7874ce26d36f41deaccbeade
Wen Xiao <wennstyle@gmail.com>
2022-02-22 22:03:31 -0500

    Done.

--- src/components/TVShow.js
+++ src/components/TVShow.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-function tvShow() {
+function TVShow(props) {
+  // console.log(props)
   return (
     <div>
       <br />
-      <img src={props.image.medium} onClick={props.selectShow} alt="" />
+      <img src={props.show.image.medium} onClick={() => props.selectShow(props.show)} alt="" />
     </div>
   );
 }
 
-export default tvShow;
+export default TVShow;
